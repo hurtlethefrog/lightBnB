e6daa1c1068516a7508565101f3020cecf474ca7
Duncan Haran <duncan.haran@gmail.com>
2019-07-24 19:45:54 +0000

    reformat schema to accept 02seed

--- migrations/01_schema.sql
+++ migrations/01_schema.sql
@@ -1,9 +1,9 @@
-DROP TABLE IF EXISTS guest_reviews;
-DROP TABLE IF EXISTS property_reviews;
-DROP TABLE IF EXISTS reservations;
-DROP TABLE IF EXISTS rates;
-DROP TABLE IF EXISTS properties;
-DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS guest_reviews CASCADE;
+DROP TABLE IF EXISTS property_reviews CASCADE;
+DROP TABLE IF EXISTS reservations CASCADE;
+DROP TABLE IF EXISTS rates CASCADE;
+DROP TABLE IF EXISTS properties CASCADE;
+DROP TABLE IF EXISTS users CASCADE;
 
 CREATE TABLE users (
   id SERIAL PRIMARY KEY,
@@ -16,44 +16,52 @@ CREATE TABLE properties (
   id SERIAL PRIMARY KEY,
   title TEXT,
   description TEXT,
-  cost_per_night SMALLINT,
-  parking_spaces SMALLINT,
-  bathrooms SMALLINT,
+  cost_per_night INTEGER,
+  parking_spaces INTEGER,
+  number_of_bathrooms INTEGER,
+  number_of_bedrooms INTEGER,
   active BOOLEAN,
-  images TEXT,
+  provence TEXT,
+  post_code TEXT,
+  street TEXT,
+  country TEXT,
+  city TEXT,
+  thumbnail_photo_url TEXT,
+  cover_photo_url TEXT,
   owner_id INT NOT NULL REFERENCES users(id) ON DELETE CASCADE
 );
 
-CREATE TABLE rates (
-  id SERIAL PRIMARY KEY,
-  start_date DATE,
-  end_date DATE,
-  rate SMALLINT,
-  property_id INT NOT NULL REFERENCES properties(id) ON DELETE CASCADE
-);
+
+-- CREATE TABLE rates (
+--   id SERIAL PRIMARY KEY,
+--   start_date DATE,
+--   end_date DATE,
+--   rate INTEGER,
+--   property_id INT NOT NULL REFERENCES properties(id) ON DELETE CASCADE
+-- );
 
 CREATE TABLE reservations (
   id SERIAL PRIMARY KEY,
   start_date DATE,
   end_date DATE,
   property_id INT NOT NULL REFERENCES properties(id) ON DELETE CASCADE,
-  user_id INT NOT NULL REFERENCES users(id) ON DELETE CASCADE
+  guest_id INT NOT NULL REFERENCES users(id) ON DELETE CASCADE
 );
 
 CREATE TABLE property_reviews (
   id SERIAL PRIMARY KEY,
   message TEXT,
-  rating SMALLINT,
+  rating INTEGER,
   reservation_id INT NOT NULL REFERENCES reservations(id) ON DELETE CASCADE,
-  user_id INT NOT NULL REFERENCES users(id) ON DELETE CASCADE,
+  guest_id INT NOT NULL REFERENCES users(id) ON DELETE CASCADE,
   property_id INT NOT NULL REFERENCES properties(id) ON DELETE CASCADE
 );
 
-CREATE TABLE guest_reviews (
-  id SERIAL PRIMARY KEY,
-  message TEXT,
-  rating SMALLINT,
-  user_id INT NOT NULL REFERENCES users(id) ON DELETE CASCADE,
-  owner_id INT NOT NULL REFERENCES users(id) ON DELETE CASCADE,
-  reservation_id INT NOT NULL REFERENCES reservations(id) ON DELETE CASCADE
-);
+-- CREATE TABLE guest_reviews (
+--   id SERIAL PRIMARY KEY,
+--   message TEXT,
+--   rating INTEGER,
+--   guest_id INT NOT NULL REFERENCES users(id) ON DELETE CASCADE,
+--   owner_id INT NOT NULL REFERENCES users(id) ON DELETE CASCADE,
+--   reservation_id INT NOT NULL REFERENCES reservations(id) ON DELETE CASCADE
+-- );
